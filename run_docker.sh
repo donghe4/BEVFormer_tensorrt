08e9d3d0d5c8b42a259e7a9ba187fb0d27fff8f0
hadonga <hadonga@kakao.com>
2024-01-17 11:39:20 +0900

    modify docker envs

--- run_docker.sh
+++ run_docker.sh
@@ -5,8 +5,10 @@ docker run -it --rm \
     -v /data/nuscenes/can_bus:/workspace/BEVFormer_tensorrt/data/can_bus \
     -v /data2/coco:/workspace/BEVFormer_tensorrt/data/coco \
     -v /data/nuscenes:/workspace/BEVFormer_tensorrt/data/nuscenes \
-    --shm-size=16G \
+    --shm-size=32G \
     --privileged \
     --network=host \
     --user root \
+    --env HTTPS_PROXY= \
+    --name BEV_TRT_DHE \
     hadonga/bev_trt:1.0 /bin/bash
--- tools/test_trt_ops.py
+++ tools/test_trt_ops.py
@@ -4,8 +4,9 @@ import sys
 
 sys.path.append(".")
 import det2trt
+from det2trt.models.utils.test_trt_ops.test_modulated_deformable_conv2d import *
+from det2trt.models.utils.test_trt_ops.test_grid_sampler import *
 from det2trt.models.utils.test_trt_ops import *
-
 
 if __name__ == "__main__":
     unittest.main()
